56cecaf011d3a0467d9936ab6a1d7618ceeb5463
poseidon0z <adityaprabhu1290@gmail.com>
2024-01-07 11:27:48 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello, World!' });
+});
+
+let todos = [];
+
+app.get('/api/todos', (req, res) => {
+  res.json(todos);
+});
+
+app.post('/api/todos', (req, res) => {
+  const newTodo = req.body.text;
+  console.log(req.body.text);
+  todos.push(newTodo);
+  res.status(201).json(newTodo);
+});
